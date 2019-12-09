c1cd45c0a6ae0385b1a2b6a8ecd7d6b43da9fa4d
adamwill1113 <58404673+adamwill1113@users.noreply.github.com>
2019-12-08 21:23:27 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import csv
 import json
+from collections import defaultdict
 
 
 headers={
@@ -9,10 +10,10 @@ headers={
 "Content-Type": "application/json"
 }
 
-#gets survey details
-url1 = "https://api.surveymonkey.net/v3/surveys/274000446/details"
+#gets question details
+url1 = "https://api.surveymonkey.net/v3/surveys/274000446/pages/104874047/questions/387014905"
 response1 = requests.get(url1, headers=headers)
-survey_details = response1.json()
+question_details = response1.json()
 
 #gets survey bulk data
 url2 = "https://api.surveymonkey.net/v3/surveys/274000446/responses/bulk"
@@ -23,43 +24,6 @@ survey_responses = response2.json()
 url3 = "https://api.surveymonkey.net/v3/surveys/274000446/pages/104874047/questions"
 response3 = requests.get(url3, headers=headers)
 survey_questions = response3.json()
-
-# Creates dictionary of question ids and titles for later remapping
-qtitle = []
-qid = []
-
-for x in survey_questions['data']:
-        for (y,z) in x.items():
-            if y == 'id':
-                qid.append(z)
-            elif y == 'heading':
-                qtitle.append(z)
-                
-qdict = dict(zip(qid, qtitle))
-
-# Creates dictionary of choice ids and text for later remapping
-#gets question details
-ctext = []
-cid = []
-
-for i in qid:
-    url1 = "https://api.surveymonkey.net/v3/surveys/274000446/pages/104874047/questions/%s" % i
-    response1 = requests.get(url1, headers=headers)
-    question_details = response1.json()
-    #print(question_details)
-    for j, k in question_details.items():
-        if j == 'answers':
-            for l, m in k.items():
-                if l == 'choices':
-                    for n in m:
-                        for z, x in n.items():
-                            if z == 'text':
-                                ctext.append(x)
-                            elif z == 'id':
-                                cid.append(x)
-
-cdict = dict(zip(cid, ctext))
-
 
 #gets list of questions and corresponding choices
 
@@ -83,9 +47,8 @@ for x in survey_responses['data']:
                                         for h, i in g.items():
                                             if h == 'choice_id':
                                                 answers.append(i)
-                                                questions.append(temp)                                    
+                                                questions.append(temp)
 
-#puts questions and answers into a dictionary and then into a dataframe
 temp = defaultdict(list)
 
 for key, value in zip(questions, answers):
@@ -95,4 +58,52 @@ adict = (dict(temp))
 
 df = pd.DataFrame({ key:pd.Series(value) for key, value in adict.items()})
 
-df
+qtitle = []
+qid = []
+
+for x in survey_questions['data']:
+        for (y,z) in x.items():
+            if y == 'id':
+                qid.append(z)
+            elif y == 'heading':
+                qtitle.append(z)
+                
+qdict = dict(zip(qid, qtitle))
+
+#gets question details
+ctext = []
+cid = []
+
+for i in qid:
+    url1 = "https://api.surveymonkey.net/v3/surveys/274000446/pages/104874047/questions/%s" % i
+    response1 = requests.get(url1, headers=headers)
+    question_details = response1.json()
+    #print(question_details)
+    for j, k in question_details.items():
+        if j == 'answers':
+            for l, m in k.items():
+                if l == 'choices':
+                    for n in m:
+                        for z, x in n.items():
+                            if z == 'text':
+                                ctext.append(x)
+                            elif z == 'id':
+                                cid.append(x)
+
+#replaces ids with text values                                
+cdict = dict(zip(cid, ctext))
+
+temp = qdict
+temp2 = adict
+
+df1 = pd.DataFrame(df)
+
+qtitle.pop()
+qtitle.pop()
+
+df1.columns = qtitle
+
+df2 = df1.replace(cdict)
+
+#write to file
+df2.to_csv(r'results.csv')
